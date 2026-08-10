d7976dc6be90b4313fe8cc6981617027484fba0b
Cosmic Crafter <61487138+theCosmicCrafter@users.noreply.github.com>
2026-08-10 00:39:57 -0400

    feat: Player tab polish - God Mode, Copy Position, Vitals GroupBox (#3.3)
    - Add God Mode button: enables NeverDie + Invulnerable + full HP/thirst/fatigue/freezing/calories
    - Add Copy Position button: copies X/Y/Z coordinates to clipboard
    - Wrap vital stats (HP, NeverDie, Invulnerable, InfiniteCarry, calories, thirst, fatigue, freezing) in a GroupBox
    - Wrap entire tab in ScrollViewer for small screens

--- The Long Dark Save Editor 2/Tabs/PlayerTab.xaml.cs
+++ The Long Dark Save Editor 2/Tabs/PlayerTab.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using The_Long_Dark_Save_Editor_2.Helpers;
 
@@ -29,5 +31,36 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
                 });
         }
 
+        private void GodModeClicked(object sender, RoutedEventArgs e)
+        {
+            var save = MainWindow.Instance?.CurrentSave;
+            if (save?.Global == null)
+                return;
+
+            save.Global.Condition.m_NeverDieProxy = true;
+            save.Global.Condition.m_Invulnerable = true;
+            save.Global.Condition.m_CurrentHPProxy = 100f;
+            save.Global.Thirst.m_CurrentThirstProxy = 100f;
+            save.Global.Fatigue.m_CurrentFatigueProxy = 100f;
+            save.Global.Freezing.m_CurrentFreezingProxy = 100f;
+            save.Global.Hunger.m_CurrentReserveCaloriesProxy = 2500f;
+
+            MainWindow.Instance.PropertyChanged?.Invoke(MainWindow.Instance, new PropertyChangedEventArgs("CurrentSave"));
+        }
+
+        private void CopyPositionClicked(object sender, RoutedEventArgs e)
+        {
+            var save = MainWindow.Instance?.CurrentSave;
+            if (save?.Global?.PlayerManager?.m_SaveGamePosition == null)
+                return;
+
+            var pos = save.Global.PlayerManager.m_SaveGamePosition;
+            var text = $"X: {pos[0]}, Y: {pos[2]}, Z: {pos[1]}";
+            try
+            {
+                Clipboard.SetText(text);
+            }
+            catch { }
+        }
     }
 }
